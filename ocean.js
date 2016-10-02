29034077285c2d7b63c034ef5feaa243d6068818
Paola Garcia <paoga.87@gmail.com>
2016-10-02 19:26:14 -0400

    Update ocean.js
    Added other exercises

--- ocean.js
+++ ocean.js
@@ -22,3 +22,25 @@ var findVehicle = function(name, list) {
 
 // call findVehicle
 findVehicle("Submarine", vehicles);
+
+var superBlinders = [
+  ["Firelight", 4000],
+  ["Solar Death Ray", 6000],
+  ["Supernova", 12000]
+];
+
+var lighthouseRock = {
+  gateClosed: true,
+  bulbs: [200, 500, 750],
+  capacity: 30,
+  secretPassageTo: "Underwater Outpost"
+};
+
+// remove bulbs property from lighthouseRock
+delete lighthouseRock.bulbs;
+
+// add weaponBulbs property to lighthouseRock
+lighthouseRock.weaponBulbs = superBlinders;
+
+// log the correct weaponBulbs array value to the console
+console.log(lighthouseRock.weaponBulbs[2][0]);
