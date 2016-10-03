4e3acfa77b787c5572b26229f2ee355714ec36ea
Paola Garcia <paoga.87@gmail.com>
2016-10-02 21:19:50 -0400

    Update ocean.js
    Added Man your Bulb Stations part

--- ocean.js
+++ ocean.js
@@ -44,3 +44,34 @@ lighthouseRock.weaponBulbs = superBlinders;
 
 // log the correct weaponBulbs array value to the console
 console.log(lighthouseRock.weaponBulbs[2][0]);
+
+// Men Your Bulb Stations Part -->
+
+var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+
+var lighthouseRock = {
+  gateClosed: true,
+  weaponBulbs: superBlinders,
+  capacity: 30,
+  secretPassageTo: "Underwater Outpost",
+  numRangers: 3,
+  ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
+  ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
+  ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1}
+};
+
+function dontPanic(location) {
+  var list = "Avast, me hearties!\n" +
+             "There be Pirates nearby! Stations!\n";
+
+  for (var i = 1; i <= location.numRangers; i++) {
+    var ranger = location["ranger"+i];
+    var name = ranger.name;
+    var superblinder = location.weaponBulbs[ranger.station-1][0];
+    list += name + ", man the " + superblinder + "!\n";
+  }
+
+  alert(list);
+}
+
+dontPanic(lighthouseRock);
